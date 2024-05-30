752875f09df6c56298fea9d4533e2ea310feb9e9
Weegoos <batyr.ashim05@mail.ru>
2024-05-30 21:42:58 +0500

    Batyr new

---
+++ src/components/FAQ.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+
+    <div class="" style="max-width: 100%; font-size: 16px;">
+    <q-list bordered v-for="faqPart in faq" :key="faqPart.id">
+      <q-expansion-item
+        group="somegroup"
+        icon="explore"
+        :label="faqPart.question"
+        header-class="bg-teal text-white"
+        expand-icon-class="text-white"
+        :caption="faqPart.caption"
+      >
+        <q-card>
+          <q-card-section>
+            {{ faqPart.answer }}
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+      <q-separator />
+    </q-list>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            faq: [
+        {
+          id: 1,
+          question: 'Что такое туристическая виза и как ее получить?',
+          answer: 'Туристическая виза - это разрешение на въезд в другую страну с целью туризма. Ее можно получить через посольство или консульство страны назначения, предоставив необходимые документы и оплатив сбор.',
+          caption: 'О сайте'
+        },
+        {
+          id: 2,
+          question: 'Как зарегистрироваться на сайте?',
+          answer: 'Для регистрации на сайте нажмите на кнопку "Регистрация" в меню, заполните необходимые поля.',
+          caption: 'О сайте'
+        },
+        {
+          id: 3,
+          question: 'Что предлагает сайт?',
+          answer: 'Наш сайт предлагает информацию о лучших туристических направлениях в Казахстане, советы по путешествиям.',
+          caption: 'О сайте'
+        },
+        {
+          id: 4,
+          question: 'Как связаться с поддержкой?',
+          answer: 'Вы можете связаться с нашей службой поддержки по электронной почте support@kazTravel.com или по телефону +7-(XXX)-XX-XX',
+          caption: 'О сайте'
+        },
+        {
+          id: 5,
+          question: 'Что делать если сайт не работает?',
+          answer: 'Попробуйте перезагрузить страницу, очистить кэш браузера или зайти позже. Если проблема не исчезает, свяжитесь с нашей службой поддержки.',
+          caption: 'Технический вопрос'
+        },
+      ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,126 +1,58 @@
 <template>
   <div class="q-px-lg q-py-md">
-    <q-timeline color="secondary">
-      <q-timeline-entry heading body="Timeline heading" />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 21, 1986"
-        icon="delete"
-        :body="body"
-      />
-
-      <q-timeline-entry heading body="November, 2017" />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        color="orange"
-        icon="done_all"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
+    <q-timeline color="secondary" >
+      <q-timeline-entry heading body="Наша история"  />
+        <q-timeline-entry v-for="historyText in history" :key="historyText.date"
+        :title="historyText.title"
+        :subtitle="historyText.date"
+        :avatar="historyText.avatar"
+        :body="historyText.body"
+        :icon="historyText.icon"
+        :color="historyText.color"
+        />
     </q-timeline>
-
-    <div class="q-pa-md" style="max-width: 350px">
-    <q-list bordered>
-      <q-expansion-item
-        group="somegroup"
-        icon="explore"
-        label="First"
-        default-opened
-        header-class="text-primary"
-      >
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-
-      <q-separator />
-
-      <q-expansion-item group="somegroup" icon="perm_identity" label="Second" header-class="text-teal">
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-
-      <q-separator />
-
-      <q-expansion-item group="somegroup" icon="shopping_cart" label="Third" header-class="text-purple">
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-
-      <q-separator />
-
-      <q-expansion-item
-        group="somegroup"
-        icon="bluetooth"
-        label="Fourth"
-        header-class="bg-teal text-white"
-        expand-icon-class="text-white"
-      >
-        <q-card class="bg-teal-2">
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-    </q-list>
-  </div>
+    <!-- FAQ -->
+    <div>
+      <FAQ />
+    </div>
   </div>
 </template>
 
 <script>
+import FAQ from '../components/FAQ.vue';
+import profile from '../assets/profile.jpg';
 export default {
-  setup () {
+  components: {
+    FAQ
+  },
+  data () {
     return {
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      history: [
+        {
+          date: 'Май 01, 2024',
+          title: 'Cоздание',
+          avatar: profile,
+          body: 'Студент Astana IT University'
+        },
+        {
+          date: 'Май 05, 2024',
+          title: 'Разработка',
+          body: 'Разрабатывался сайт'
+        },
+        {
+          date: 'Май 12, 2024',
+          title: 'Команда',
+          icon: 'style',
+          body: 'Командная работа'
+        },
+        {
+          date: 'Июнь 1, 2024',
+          title: 'Завершение',
+          icon: 'done_all',
+          body: 'Закончили проект',
+          color: 'orange'
+        },
+      ]
     }
   }
 }
